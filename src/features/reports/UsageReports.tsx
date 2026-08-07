4639f54bf51f254c708440c1c6b9775e5744c212
ketanmodi087 <ketan.kanhasoft@gmail.com>
2026-08-07 11:11:50 +0530

    feat(reports): add reports module

---
+++ src/features/reports/UsageReports.tsx
@@ -0,0 +1,10 @@
+import { UsageReport } from "molecules";
+
+const UsageReports = () => {
+
+  return (
+    <UsageReport />
+  );
+};
+
+export default UsageReports;
---
+++ src/features/reports/index.ts
@@ -0,0 +1,1 @@
+export { default as UsageReports } from "./UsageReports";
