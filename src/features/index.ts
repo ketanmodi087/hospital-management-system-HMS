5e9c88a2552064db2b4dc192a6713adb387b5681
ketanmodi087 <ketan.kanhasoft@gmail.com>
2026-08-17 17:55:33 +0530

    feat(features): add features index

---
+++ src/features/index.ts
@@ -0,0 +1,7 @@
+export * from "./auth";
+export * from "./clinical";
+export * from "./dashboard";
+export * from "./marketing";
+export * from "./operations";
+export * from "./reports";
+export * from "./tenant-management";
